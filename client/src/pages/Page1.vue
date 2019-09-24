3f8f8e3b07cb98dc53c5c1a2e8e71b57f7a6284d
Kelly Ferrone <4399427+kferrone@users.noreply.github.com>
2019-09-24 12:48:57 -0600

    finally got the full ci build working

--- client/src/pages/Page1.vue
+++ client/src/pages/Page1.vue
@@ -4,11 +4,11 @@
   ul
     li
       router-link( to="/") Home
-  RichTextEditor(value="<p>Hello</p>")
+  RichTextEditor(value="<p>Hello World</p>")
 </template>
 <script lang="coffee">
 import RichTextEditor from '../components/RichTextEditor'
-export default 
+export default
   name: 'Page1'
   components: {
     RichTextEditor
